2b2f7f9b7694637444baf12030824f4d71428cce
HongZeBin98 <694546230@qq.com>
2018-08-01 16:17:51 +0800

    fix(PicureListAdapter):修复了recylerView网格布局只每页只显示一行的bug， 修改方法：把其adapter的inflate方法中第二个参数由parent改成null

--- davinci/src/main/java/com/example/davinci/adapter/PictureListAdapter.java
+++ davinci/src/main/java/com/example/davinci/adapter/PictureListAdapter.java
@@ -50,7 +50,7 @@ public class PictureListAdapter extends RecyclerView.Adapter<PictureListAdapter.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview, null, false);
         return new ViewHolder(view);
     }
 
